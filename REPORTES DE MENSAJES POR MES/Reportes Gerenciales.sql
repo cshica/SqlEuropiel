42867ba51e66882a87c4bd30893a9675c019cf2e
cshica <cshica@gmail.com>
2021-05-10 09:12:43 -0500

    A

--- REPORTES DE MENSAJES POR MES/Reportes Gerenciales.sql
+++ REPORTES DE MENSAJES POR MES/Reportes Gerenciales.sql
@@ -1,7 +1,9 @@
 -----------------------------------------------------------------------------------------------------------------------
 -- ENVIOS DE MENSAJES POR MES
 -----------------------------------------------------------------------------------------------------------------------
-
+--ENERO
+select sum(isnull(EnviosPorMes,0)) envios ,sum(Costo) monto from PLANTILLA_DETALLE where Mes=1 union 
+select count( *),sum(Price)   from Paso2_Enero where   From_ in( select t.NumeroWhatsapp from WhatsappEmisor t)
 --FEBRERO
 select sum(isnull(EnviosPorMes,0)) envios ,sum(Costo) monto from PLANTILLA_DETALLE where Mes=2 union 
 select count( *),sum(Price)   from Paso2_febrero where   From_ in( select t.NumeroWhatsapp from WhatsappEmisor t)
@@ -15,6 +17,13 @@ select count( *),sum(Price)   from Paso2_abril where   From_ in( select t.Numero
 -----------------------------------------------------------------------------------------------------------------------
 -- MENSAJES POR GRUPOS
 -----------------------------------------------------------------------------------------------------------------------
+DROP TABLE IF EXISTS #ENERO
+select PD.*,G.IdGrupo,G.Descripcion INTO #ENERO from PLANTILLA_DETALLE PD
+INNER JOIN PLANTILLA P ON P.IdPlantilla= PD.IdPlantilla
+INNER JOIN GRUPO G ON G.Idgrupo=P.IdGrupo
+where Mes=1
+order by G.IdGrupo
+
 DROP TABLE IF EXISTS #FEBRERO
 select PD.*,G.IdGrupo,G.Descripcion INTO #FEBRERO from PLANTILLA_DETALLE PD
 INNER JOIN PLANTILLA P ON P.IdPlantilla= PD.IdPlantilla
@@ -22,4 +31,27 @@ INNER JOIN GRUPO G ON G.Idgrupo=P.IdGrupo
 where Mes=2
 order by G.IdGrupo
 
-SELECT SUM(EnviosPorMes),SUM(Costo) FROM #FEBRERO
+DROP TABLE IF EXISTS #MARZO
+select PD.*,G.IdGrupo,G.Descripcion INTO #MARZO from PLANTILLA_DETALLE PD
+INNER JOIN PLANTILLA P ON P.IdPlantilla= PD.IdPlantilla
+INNER JOIN GRUPO G ON G.Idgrupo=P.IdGrupo
+where Mes=3
+order by G.IdGrupo
+
+DROP TABLE IF EXISTS #ABRIL
+select PD.*,G.IdGrupo,G.Descripcion INTO #ABRIL from PLANTILLA_DETALLE PD
+INNER JOIN PLANTILLA P ON P.IdPlantilla= PD.IdPlantilla
+INNER JOIN GRUPO G ON G.Idgrupo=P.IdGrupo
+where Mes=4
+order by G.IdGrupo
+
+SELECT SUM(EnviosPorMes) MENSAJES ,SUM(Costo) MONTO, Descripcion FROM #ENERO  /*WHERE Descripcion='Confirmación de citas'*/ GROUP BY Descripcion    
+union select sum(M.EnviosPorMes),sum(M.Costo), 'Total'  from #ENERO M  order by Descripcion asc 
+SELECT SUM(EnviosPorMes) MENSAJES ,SUM(Costo) MONTO, Descripcion FROM #FEBRERO  /*WHERE Descripcion='Confirmación de citas'*/ GROUP BY Descripcion  
+union select sum(M.EnviosPorMes),sum(M.Costo), 'Total'  from #FEBRERO M  order by Descripcion asc
+SELECT SUM(EnviosPorMes) MENSAJES ,SUM(Costo) MONTO, Descripcion FROM #MARZO    /*WHERE Descripcion='Confirmación de citas'*/ GROUP BY Descripcion  
+union select sum(M.EnviosPorMes),sum(M.Costo), 'Total'  from #MARZO M  order by Descripcion asc
+SELECT SUM(EnviosPorMes) MENSAJES ,SUM(Costo) MONTO, Descripcion FROM #ABRIL    /*WHERE Descripcion='Confirmación de citas'*/ GROUP BY Descripcion  
+union select sum(M.EnviosPorMes),sum(M.Costo), 'Total'  from #ABRIL M order by Descripcion asc   
+
+ 
--- REPORTES DE MENSAJES POR MES/listar montos por mensajes enviados.sql
+++ REPORTES DE MENSAJES POR MES/listar montos por mensajes enviados.sql
@@ -1,6 +1,6 @@
 drop table if exists #tabla
-select  *  into #tabla from Paso2_abril where   From_ in( select t.NumeroWhatsapp from WhatsappEmisor t)
-SELECT Body FROM #tabla ORDER BY Body ASC
+select  *  into #tabla from Paso2_Enero where   From_ in( select t.NumeroWhatsapp from WhatsappEmisor t)
+SELECT Body FROM #tabla where Body like '%fue cancelada por error, por favor haz caso omiso a dicha cancelación y acude a tu cita como estaba originalmente programado, te esperamos para atenderte lo mejor posible!.' ORDER BY Body ASC
 --Tu OTP es 00184413
 --------------------------------------------------------------------------------------------------------------------------------------------
 declare @i int=1
@@ -13,23 +13,25 @@ declare @id int =1
 	declare @limite int =( select max(IdPlantilla)+1 from PLANTILLA)
 while (@i<@limite)
 begin
-	set @filtro =(select Body from PLANTILLA where IdPlantilla=@i)
+    if not exists(select * from PLANTILLA_DETALLE where IdPlantilla=@i)
+    begin
+        set @filtro =(select Body from PLANTILLA where IdPlantilla=@i)
 
-	drop table if exists #tabla_total
-	select  body, count(*) cantidad ,abs(sum(Price)) precio into #tabla_total from #tabla  
-	where    
-	Body like @filtro
-	group by Body
-	order by body asc
+        drop table if exists #tabla_total
+        select  body, count(*) cantidad ,abs(sum(Price)) precio into #tabla_total from #tabla  
+        where    
+        Body like @filtro
+        group by Body
+        order by body asc
 
-	--select * from #tabla_total
-	select @ENVIOS= sum(cantidad),@COSTO= sum(precio) from #tabla_total
+        --select * from #tabla_total
+        select @ENVIOS= sum(cantidad),@COSTO= sum(precio) from #tabla_total
 
 
-	delete from #tabla where Body like @filtro
-	--INSERT INTO PLANTILLA (Body) VALUES (@filtro)
-	SET @id=@i
-	INSERT INTO PLANTILLA_DETALLE (IdPlantilla,EnviosPorMes,Mes,Costo) VALUES(@ID,@ENVIOS,4,@COSTO)
-
+        delete from #tabla where Body like @filtro
+        --INSERT INTO PLANTILLA (Body) VALUES (@filtro)
+        SET @id=@i
+        INSERT INTO PLANTILLA_DETALLE (IdPlantilla,EnviosPorMes,Mes,Costo) VALUES(@ID,isnull(@ENVIOS,0),1,isnull(@COSTO,0))
+    end
 	set @i=@i+1
 end
