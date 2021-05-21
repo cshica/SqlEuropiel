a2da91af3dad7d0c88572d9dd5fc0343c6601ffa
cshica <christianshica@europiel.com.mx>
2021-05-21 16:01:26 -0500

    mensajes

--- PROCESOS/CONFIRMACION_CITA_WHATSAPP/revisar.sql
+++ PROCESOS/CONFIRMACION_CITA_WHATSAPP/revisar.sql
@@ -1,6 +1,6 @@
-select * from rm_europiel_guadalajara.dbo.TABLA_NOTIFI_WHATSAPP 
-where --cast(HORA_EJECUCION as date)=cast(GETDATE() as date)
-telefono like '%4423434013%'
+select * from rm_europiel.dbo.TABLA_NOTIFI_WHATSAPP 
+where cast(HORA_EJECUCION as date)=cast(GETDATE() as date)
+--telefono like '%4423434013%'
 order by HORA_EJECUCION desc
 --select * from #tabla
 
@@ -8,14 +8,9 @@ select * from rm_europiel_requerimientos.dbo.notifier_mensajes
 where 
 mobile_os='whatsapp'
 and id_notifier=5
-and cast(fecha_alta_registro as date)=cast(GETDATE() as date)
-and (payload   not like '%preferenica%'
-or  payload  not like '%Tu número de acceso%' 
-or  payload  not like '%Actualiza tu APP%' 
-or  payload  not like '%hemos terminado%' 
-or  payload  not like '%Agradecemos tu confianza al tomar el tratamiento%' 
-or  payload  not like '%Gracias por tu preferencia.%' 
-)
+and cast(fecha_alta_registro as date)=cast(GETDATE()  as date)
+and payload like '%Hola!, tu cita para depilarte se aproxima%'
+
 --and bloque not like '%ESP%'
 --and id_usuario=19584
 order by fecha_alta_registro,id_usuario desc
