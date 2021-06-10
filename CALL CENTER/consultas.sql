769e91cc37670263ebf3638f58684d00961053da
cshica <christianshica@europiel.com.mx>
2021-06-10 17:55:26 -0500

    acivar y desactivar call center

---
+++ CALL CENTER/consultas.sql
@@ -0,0 +1,70 @@
+--select * from CONFIGURACIONES_MENSAJES_TWILIO
+DROP TABLE IF EXISTS CONFIG_TWILIO
+CREATE TABLE CONFIG_TWILIO
+(
+	Id int
+	,Descripcion NVARCHAR(max)
+	,N1 INT
+	,N2 INT
+	,N3 INT
+	,Str1 NVARCHAR(max)
+	,Str2 NVARCHAR(max)
+	,Str3 NVARCHAR(max)
+)
+GO
+INSERT INTO CONFIG_TWILIO(Id,Descripcion,N1)
+VALUES (1,'1=Activa Call Center, 0= Desactiva Call Center, es invocado desde ',1)
+INSERT INTO CONFIG_TWILIO(Id,Descripcion,Str1)
+values (2,'Rol que permite activar o desactivar Call Center','supervisor'),(2,'Rol que permite activar o desactivar Call Center','admin')
+GO
+SELECT * FROM CONFIG_TWILIO
+go
+DROP PROC IF EXISTS ActivarLlamadasCallCenter
+GO
+CREATE PROCEDURE ActivarLlamadasCallCenter -- ActivarLlamadasCallCenter 0
+(
+	@ACCION int
+)
+AS
+BEGIN
+	IF @ACCION=0 
+	BEGIN
+		UPDATE CONFIG_TWILIO SET N1=0 WHERE Id=1
+	END
+	IF @ACCION=1
+	BEGIN
+		UPDATE CONFIG_TWILIO SET N1=1 WHERE Id=1
+	END
+END
+GO
+DROP PROC IF EXISTS GetStatusCallCenter
+GO
+CREATE PROCEDURE GetStatusCallCenter --GetStatusCallCenter
+(
+	@result bit=0 output
+)
+AS
+BEGIN	
+
+	SET @result=(SELECT N1 FROM CONFIG_TWILIO WHERE Id=1)
+	print @result
+	return @result
+END
+GO
+DROP PROCEDURE IF EXISTS ValidarRolCallCenter
+GO
+CREATE PROCEDURE ValidarRolCallCenter -- ValidarRolCallCenter 'admin1'
+(
+	@rol nvarchar(50)
+	,@result bit =0 output
+)
+AS
+BEGIN
+	IF EXISTS(	SELECT * FROM CONFIG_TWILIO where id=2 and Str1=@rol)
+		SET @result=1
+	ELSE
+		SET @result=0
+
+	PRINT @result
+	RETURN @result
+END
